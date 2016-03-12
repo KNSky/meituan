40627369d959f7e828bd74865fdf3fcdf9015831
KNSky <182520598@qq.com>
2016-03-12 20:07:07 +0800

    找到素材了

--- MeiTuan/MeiTuan/ViewController.m
+++ MeiTuan/MeiTuan/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
      NSLog(@"%@==>  我是张三 我排名第二2222",[self class]);
      NSLog(@"%@==>  开发一个zzzzzz 1.0",[self class]);
+     NSLog(@"%@==>  感觉不错,现在开始搞起",[self class]);
 }
 
 - (void)didReceiveMemoryWarning {
